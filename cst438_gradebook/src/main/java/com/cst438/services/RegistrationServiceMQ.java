479ac784886fd0adebc491b48bcdaa6cacc7fa9b
Georgekorolev <gkorolev@csumb.edu>
2023-04-11 10:01:37 -0700

    service REST and MQ implemented

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -1,11 +1,14 @@
 package com.cst438.services;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
@@ -25,6 +28,9 @@ public class RegistrationServiceMQ extends RegistrationService {
 
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
+	
+	@Value("${registration.url}") 
+	String registration_url;
 
 	public RegistrationServiceMQ() {
 		System.out.println("MQ registration service ");
@@ -45,6 +51,14 @@ public class RegistrationServiceMQ extends RegistrationService {
 	public void receive(EnrollmentDTO enrollmentDTO) {
 		
 		//TODO  complete this method in homework 4
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+		if(c == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course id not found. GK");
+		Enrollment e = new Enrollment();
+		e.setStudentEmail(enrollmentDTO.studentEmail);
+		e.setStudentName(enrollmentDTO.studentName);
+		e.setCourse(c);
+		e = enrollmentRepository.save(e);
+		enrollmentDTO.id = e.getId();
 		
 	}
 
@@ -53,6 +67,8 @@ public class RegistrationServiceMQ extends RegistrationService {
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
 		 
 		//TODO  complete this method in homework 4
+		System.out.println("Enrollment DTO:" + courseDTO);
+		rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
 		
 	}
 
